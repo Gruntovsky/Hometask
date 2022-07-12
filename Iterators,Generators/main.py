b1fce61ab1b0e8a8bfbfa723a8164e76072bc4f8
gruntovsky <G.J.Gruntovsky@yandex.ru>
2022-07-12 16:37:02 +0300

    ДЗ по итераторам,генераторам

--- Iterators,Generators/main.py
+++ Iterators,Generators/main.py
@@ -1,34 +1,35 @@
 from data import nested_list
+from itertools import chain
+
 print()
 print('Iterator')
+
+
 class FlatIterator:
 
     def __init__(self, some_list):
         self.main_list = some_list
 
     def __iter__(self):
-        self.main_list_cursor = 0  # курсор основного списка
-        self.nested_list_cursor = 0 # курсор списка вложенного в основной список
+        self.main_list_cursor = -1  # РєСѓСЂСЃРѕСЂ РѕСЃРЅРѕРІРЅРѕРіРѕ СЃРїРёСЃРєР°
+        self.nested_list_cursor = 0  # РєСѓСЂСЃРѕСЂ СЃРїРёСЃРєР° РІР»РѕР¶РµРЅРЅРѕРіРѕ РІ РѕСЃРЅРѕРІРЅРѕР№ СЃРїРёСЃРѕРє
         return self
 
     def __next__(self):
-        if self.main_list_cursor == len(nested_list):
-            raise StopIteration
-        elif self.nested_list_cursor == len(nested_list[self.main_list_cursor]):
-             self.main_list_cursor += 1
-             self.nested_list_cursor = 0
-        else:
-            position = nested_list [self.main_list_cursor][self.nested_list_cursor]
+        self.main_list_cursor += 1
+        if self.main_list_cursor == len(self.main_list[self.nested_list_cursor]):
+            self.main_list_cursor = 0
             self.nested_list_cursor += 1
-            return position
+        if self.nested_list_cursor == len(self.main_list):
+            raise StopIteration
+        return self.main_list[self.nested_list_cursor][self.main_list_cursor]
 
+if __name__ == '__main__':
+    flat_list = []
+    for item in FlatIterator(nested_list):
+        flat_list.append(item)
+    print(flat_list)
 
-
-flat_list = []
-for item in FlatIterator(nested_list):
-   flat_list.append(item)
-print(flat_list)
-#
 print()
 print('Generator')
 def FlatGenerator(nested_list):
@@ -39,7 +40,7 @@ a = FlatGenerator(nested_list)
 for x in a:
     for result in x:
         print(result)
-        print(result)
+
 
 
 
